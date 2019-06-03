a40db222e7217c033f964fbaf2f6f2b0629c131e
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-03 22:50:28 +0100

    Integrated db response in rb

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -4,40 +4,22 @@ class Bookmark
   attr_reader :list
 
   def initialize
-    @list = [1]
+    @list = []
   end
 
   def all
-    # @list = ['bbc', 'makersacademy']
-    
     begin 
       con = PG.connect :dbname => 'bookmark_manager', :user => 'linna@paddle.com'
       
       rs = con.exec "SELECT * FROM bookmarks"
   
       rs.each do |row|
-        item = "%s" % [row['url']]
-        @list << item
+        @list << "%s" % [row['url']]
       end
     end
   end
-
-  b = Bookmark.new
-  puts b
-  b.all
-  puts b.list
-  
-  # puts "*************"
-  # begin 
-  #   con = PG.connect :dbname => 'bookmark_manager', :user => 'linna@paddle.com'
-    
-  #   rs = con.exec "SELECT * FROM bookmarks"
-
-  #   rs.each do |row|
-  #     puts "%s" % [row['url']]
-  #   end
-  # end
-
-  # puts @list
-  # puts "*************"
 end
+
+# b = Bookmark.new
+# b.all
+# pp b.list
